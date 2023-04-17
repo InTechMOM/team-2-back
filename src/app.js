dfae51908d4d209d5932d7af006ece6bc9cda741
Julissa Espinosa <julissa.espinosa.cabrales@gmail.com>
2023-04-17 13:01:57 -0600

    add mongo connection

--- src/app.js
+++ src/app.js
@@ -4,6 +4,23 @@ import { port } from './config/index.js';
 
 
 const app = express();
+
+//Conexion a la base de Datos
+const mongose=requiere(mongodb).mongose;
+const uri='';
+
+mongose.connect(uri, (err,db)=> {
+if(err) throw err
+
+const dbo=db.db("mydb");
+
+dbo.collection('Collection name').find().toArray((err,result)=>{
+if (err) throw err
+console.log(result)
+  })
+})
+
+
 app.get('/', (request, response, error) => {
   response.send('Status: OK')
 })
