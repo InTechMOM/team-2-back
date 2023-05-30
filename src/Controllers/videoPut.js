ebc305a7d1ca06fe3c77709b256b26a135354a09
Julissa Espinosa <julissa.espinosa.cabrales@gmail.com>
2023-05-29 18:16:16 -0600

    change API PUT for send mutiples skills

--- src/Controllers/videoPut.js
+++ src/Controllers/videoPut.js
@@ -62,28 +62,27 @@ import Video from '../Models/videoProject.js';
  */
 
 const videoPutAssesment=async(request,response,next)=>{
-  const videoId = request.params.id;
-  const newAssessment = request.body.assessment;
-try {
-   // Buscar el video por ID
-   const existingVideo = await Video.findById(videoId);
 
-   if (!existingVideo) {
-    return response.status(404).json({ error: 'Video not found' });
+  
+  
+  try {
+    const videoId = request.params.id;
+    const { assessments } = request.body;
+
+    const video = await Video.findById(videoId);
+
+    if (!video) {
+      return response.status(404).json({ message: 'Video not found' });
+    }
+
+    video.assessments.push(...assessments);
+    await video.save();
+
+    response.status(200).json({ message: 'Assessments updated successfully', data: video });
+  } catch (error) {
+    next(error);
   }
-  
-   // Agregar las nuevas calificaciones al objeto de calificaciones existente
-   existingVideo.assessments.push(newAssessment);
-  
-    
-    
-   // Guardar el video actualizado
-   const updatedVideo = await existingVideo.save();
 
-   response.status(200).json(updatedVideo);
   
-} catch (error) {
-  next(error);
-}
 };
 export default videoPutAssesment;
